bb7cad4e96aef542b3e279d6ebf2c6a9d2e7914a
MarcoGotti <mrc.gotti@gmail.com>
2023-12-05 22:54:23 +0000

    fix Pari Dispari

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,13 +44,43 @@ if (word === drow) {
 
 //pick even  or odd
 const userChoice = prompt('write E for Even or O for Odd');
+
 //pick a numb 1 to 5
 const userNumber = Number(prompt('type a number 1-5'));
 console.log(userNumber);
-//generate random number 1-5
+
+//generate random number 1-5 with a function
 function randomNumber(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
+
+const rdmNumber = (randomNumber(1, 5));
+console.log(rdmNumber);
+console.log(rdmNumber + userNumber);
+
+//Sum the two numbers and determine if sum is even or odd
+function sum(num_1, num_2){
+    return ((num_1 + num_2) % 2)
+}
+
+sum(userNumber, rdmNumber);
+const modulus = sum(userNumber, rdmNumber);
+console.log(modulus);
+
+//declare the winner
+
+if (((modulus == 0) && (userChoice === 'E')) || ((modulus !== 0) && (userChoice === 'O'))) {
+    //const rdmSum = userNumber + rdmNumber;
+    console.log('you win! ' + (userNumber + rdmNumber) + ' is ' + userChoice);
+} else {
+    //const rdmSum = userNumber + rdmNumber;
+    console.log('you lose! ' + (userNumber + rdmNumber) + ' is not ' + userChoice);
+}
+
+
+
+/* 
+FIRST ATTEMPT it works but messy
 
 //console.log(randomNumber(1, 5));
 const rdmNumber = (randomNumber(1, 5));
@@ -71,3 +101,4 @@ function modulusDivision(rdmSum){
 
 const modulus = (modulusDivision(rdmSum));
 console.log(modulus);
+ */
